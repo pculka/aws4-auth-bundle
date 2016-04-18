5fc4c692d1705cda96db2fa230ed251d25feb29a
Peter Culka <peter.culka@primeinteractive.net>
2016-04-18 13:06:17 +0200

    add DependencyInjection

---
+++ DependencyInjection/PCAws4AuthExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PC\Aws4AuthBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class PCAws4AuthExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
