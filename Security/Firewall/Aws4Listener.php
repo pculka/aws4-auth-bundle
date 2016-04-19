1a1f463db5ecf3d77b208489b77af44cddd1ace9
Peter Culka <peter.culka@primeinteractive.net>
2016-04-19 12:39:29 +0200

    calculate hashed canonical header

--- Security/Firewall/Aws4Listener.php
+++ Security/Firewall/Aws4Listener.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Aws\Signature\SignatureV4;
 #use AppBundle\Security\Authentication\Token\WsseUserToken;
 
 class Aws4Listener implements ListenerInterface
@@ -33,16 +35,56 @@ class Aws4Listener implements ListenerInterface
         //$aws4Regex = '/Authorization: AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20150830/us-east-1/iam/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=5d672d79c15b13162d9279b0855cfba6789a8edb4c82c400e06b5924a6f2b5d7';
         //$aws4Regex = '/([^"]+) Credential=([^"]+), SignedHeaders=([^"]+), Signature=([a-z9-0^"]+)/';
         // AWS4-HMAC-SHA256 Credential=key1/20160418/us-east-1/S3/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=9f48d763c1cb38c08d5ae52ea27043dca987a66d3b289fb3dedb606305fd9d55
-        $aws4Regex = '/AWS4-HMAC-SHA256 Credential=(.+), SignedHeaders=([a-z\-;]+), Signature=([a-z0-9]+)/';
+        $aws4Regex = '/AWS4-HMAC-SHA256 Credential=(.+), SignedHeaders=([a-z0-9\-;]+), Signature=([a-z0-9]+)/';
 
-
+        // TODO: add also query string signature alternative (http://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html)
         if (!$request->headers->has('Authorization') || 1 !== preg_match($aws4Regex, $request->headers->get('Authorization'), $matches)) {
             return;
         }
 
-        list($credential['key'], $credential['date'], $credential['zone'], $credential['service']) = explode('/', $matches[1]);
+        list($credential['key'], $credential['date'], $credential['region'], $credential['service']) = explode('/', $matches[1]);
         $signedHeaders = explode(';', $matches[2]);
         $signature = $matches[3];
+
+        // Pseudocode for deriving a signing key
+        // TODO: GET THE SECRET FROM THE USER WITH CREDENTIAL KEY
+        $kSecret = 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY';
+
+        $kDate = hash_hmac("sha256", $credential['date'], "AWS4".$kSecret, true);
+        $kRegion = hash_hmac("sha256", $credential['region'], $kDate, true);
+        $kService = hash_hmac("sha256", $credential['service'], $kRegion, true);
+        $kSigning = hash_hmac("sha256", "aws4_request", $kService, true); // this is the key we use to sign the request
+
+        // create Canonical Headers
+        $originalHeaders = array();
+        sort($signedHeaders, SORT_STRING);
+        foreach ($signedHeaders as $signedHeader) {
+            $originalHeaders[] = strtolower($signedHeader).':'.trim($request->headers->get($signedHeader));
+        }
+
+        $canonicalHeaders = implode("\n", $originalHeaders);
+
+        $parsedUrl = parse_url($request->getUri());
+        $canonicalUri = (trim($parsedUrl['path']) == "")? "/" : $parsedUrl['path'];
+        $canonicalQueryString = $request->getQueryString();
+        $hashedPayload = strtolower(hash('sha256', $request->getContent()));
+        $canonicalRequest =
+            $request->getMethod()."\n".
+            $canonicalUri."\n".
+            $canonicalQueryString."\n".
+            $canonicalHeaders."\n".
+            implode(";", $signedHeaders)."\n".
+            $hashedPayload
+            ;
+        $hashedCanonicalRequest = strtolower(hash('sha256', $canonicalRequest));
+        // todo: check $hashedPayload == $request->headers->get('x-amz-content-sha256'));
+//        var_dump($signature);
+//        var_dump($canonicalRequest);
+//        var_dump($hashedCanonicalRequest);
+
+        // $ro = preg_replace('/\s+/', ' ',$row['message']);
+
+
 
         //$password = ...;
 
